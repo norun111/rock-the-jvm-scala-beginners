9b833871a05a1baeb0efb5db67fa3e2885d66eaa
tomoyaueno29 <u.t012807@gmail.com>
2020-07-13 20:51:56 +0900

    Update InheritanceAndTraits.scala

--- src/lectures/Part2oop/InheritanceAndTraits.scala
+++ src/lectures/Part2oop/InheritanceAndTraits.scala
@@ -1,13 +1,50 @@
 package lectures.Part2oop
 
-object InheritanceAndTraits {
+object InheritanceAndTraits extends App{
 
+  //single class inheritance
   class Animal {
-    def eat = println("nomnom")
+    val creatureType = "wild"
+    protected def eat = println("nomnom")
+  }
+  class Cat extends Animal {
+    def crunch = {
+      eat
+      println("crunch crunch")
+    }
   }
 
-  class Cat extends Animal
-
   val cat = new Cat
-  cat.eat
+//  cat.eat can't do this (protected)
+  cat.crunch
+
+  //not override yet
+  println(cat.creatureType)
+
+  //construction
+  class Person(name: String, age: Int) {
+    def this(name: String) = this(name, 0)
+  }
+  class Adult(name: String, age: Int, idCard: String) extends Person(name)
+
+  //overriding
+//  class Dog(override val creatureType: String) extends Animal {
+////    override val creatureType: String = "domestic"
+//    override def eat = println("crunch crunch")
+//  }
+
+  class Dog(dogType: String) extends Animal {
+    override val creatureType: String = dogType
+    override def eat = println("crunch crunch")
+  }
+
+  val dog = new Dog("K9")
+  dog.eat
+  println(dog.creatureType)
+
+  // type substitution
+  val unknownAnimal = new Dog("K9")
+
+  unknownAnimal.eat
+
 }
