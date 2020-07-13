a59cacb6b81bc64304afcc848f09a9c323582575
tomoyaueno29 <u.t012807@gmail.com>
2020-07-13 20:35:31 +0900

    Update Objects.scala

--- src/lectures/Part2oop/Objects.scala
+++ src/lectures/Part2oop/Objects.scala
@@ -1,29 +1,39 @@
 package lectures.Part2oop
 
-object Objects extends App {
+object Objects {
 
   // SCALA DOES NOT HAVE CLASS-LEVEL FUNCTIONALITY ("static")
   object Person { // type + its only instance
     // "static"/"class" - level functionality
     val N_EYES = 2
     def canFly: Boolean = false
+
+    //factory method
+    def apply(mother: Person, father: Person): Person = new Person("Bobbie")
   }
 
-  class Person {
+  class Person(name: String) {
     // instance-level functionality
   }
 
   // COMPANIONS
+  def main(args: Array[String]): Unit = {
+    println(Person.N_EYES)
+    println(Person.canFly)
 
-  println(Person.N_EYES)
-  println(Person.canFly)
+    // Scala objects = SINGLETON INSTANCE
+    val mary = new Person("Mary")
+    val john = new Person("John")
+    println(mary == john)
 
-  // Scala objects = SINGLETON INSTANCE
-  val mary = new Person
-  val john = new Person
-  println(mary == john)
+    val person1 = Person
+    val person2 = Person
+    println(person1 == person2)
 
-  val person1 = Person
-  val person2 = Person
-  println(person1==person2)
+    val bobbie = Person(mary, john)
+  }
+
+  // Scala Applications = Scala object with
+  // def main(args: Array[String]): Unit
+
 }
