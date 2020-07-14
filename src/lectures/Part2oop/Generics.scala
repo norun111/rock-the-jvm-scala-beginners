8d57e6a67afd5e482a6601595e4a41279573f416
tomoyaueno29 <u.t012807@gmail.com>
2020-07-14 14:46:05 +0900

    Create Generics.scala

---
+++ src/lectures/Part2oop/Generics.scala
@@ -0,0 +1,53 @@
+package lectures.Part2oop
+
+object Generics extends App {
+
+  class MyList[+A] {
+    // use the type A
+    def add[B >: A](element: B): MyList[B] = ???
+    /*
+     A = Cat
+     B = Dog = Animal
+     */
+
+  }
+
+  class MuMap[Key, Value] //[Generic Type]
+  val listOfInteger = new MyList[Int]
+  val listOfStrings = new MyList[String]
+
+  // generics methods
+  object MyList {
+    def empty[A]: MyList[A] = ???
+  }
+  val emptyListOfIntegers = MyList.empty[Int]
+
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // 1. yes List[Cat] extends List[Animal] = COVARIANCE
+  class CovariantList[+A]
+  val animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+  // animalList.add(new Dog) ??? HARD QUESTION => we return a list of Animal
+
+  // 2. NO = INVARIANCE
+  class InvariantList[A]
+  val InvariantAnimalList: InvariantList[Animal] = new InvariantList[Animal]
+
+  // 3. Hell, no! CONTRAVARIANCE
+  class Trainer[-A]
+  val contravariantList: Trainer[Cat] = new Trainer[Animal]
+
+
+  // bounded types
+  class Cage[A >: Animal](animal: A)
+  val cage = new Cage(new Dog)
+
+  class Car
+  val newCage = new Cage(new Car)
+
+  // expand MyList to be generics
+}
